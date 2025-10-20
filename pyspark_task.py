015651517811ce65d788cebaeb56b966f6a68d80
Dmitry Korablin <korablindm@gmail.com>
2025-10-20 16:52:08 +0300

    checked with pylint

--- pyspark_task.py
+++ pyspark_task.py
@@ -1,14 +1,16 @@
-from pyspark.sql import SparkSession, DataFrame
+"""Used for function definitions"""
 from typing import Tuple
-
+from pyspark.sql import SparkSession, DataFrame
 
 
 def get_spark_session() -> SparkSession:
+    """Returns prepared SparkSession for tests"""
     return SparkSession.builder.appName("PySpark 101").getOrCreate()
 
 
-def get_test_dataframes(spark: SparkSession) -> Tuple[DataFrame, DataFrame, DataFrame]:
-    products_df = spark.createDataFrame([
+def get_test_dataframes(sparksess: SparkSession) -> Tuple[DataFrame, DataFrame, DataFrame]:
+    """Returns prepared DataFrames for tests"""
+    products_df = sparksess.createDataFrame([
         ("Product A", 1),
         ("Product B", 2),
         ("Product C", 3),
@@ -16,13 +18,13 @@ def get_test_dataframes(spark: SparkSession) -> Tuple[DataFrame, DataFrame, Data
         ("Product E", 5),
     ], schema=["prod_name", "prod_id"])
 
-    categories_df = spark.createDataFrame([
+    categories_df = sparksess.createDataFrame([
         ("Category 1", 1),
         ("Category 2", 2),
         ("Category 3", 3),
     ], schema=["cat_name", "cat_id"])
 
-    links_df = spark.createDataFrame([
+    links_df = sparksess.createDataFrame([
         (1, 1),
         (2, 2),
         (3, 2),
@@ -32,7 +34,8 @@ def get_test_dataframes(spark: SparkSession) -> Tuple[DataFrame, DataFrame, Data
 
 
 # The desired method
-def get_pairs(spark: SparkSession, products_df: DataFrame, categories_df: DataFrame, links_df: DataFrame) -> Tuple[DataFrame, DataFrame]:
+def get_pairs(products_df: DataFrame, categories_df: DataFrame, links_df: DataFrame) \
+                -> Tuple[DataFrame, DataFrame]:
     """
     Parameters:
         spark - active spark session
@@ -44,7 +47,8 @@ def get_pairs(spark: SparkSession, products_df: DataFrame, categories_df: DataFr
         df1 - DataFrame with each prod_name-cat_name pair 
         df2 - names (prod_name) of all products without a single category
     """
-    df1 = links_df.join(products_df, "prod_id").join(categories_df, "cat_id").select("prod_name", "cat_name")
+    df1 = links_df.join(products_df, "prod_id").join(categories_df, \
+        "cat_id").select("prod_name", "cat_name")
     df2 = products_df.join(links_df, "prod_id", "left")
     df2 = df2.filter(df2["cat_id"].isNull()).select("prod_name")
     return df1, df2
@@ -52,8 +56,8 @@ def get_pairs(spark: SparkSession, products_df: DataFrame, categories_df: DataFr
 
 if __name__ == "__main__":
     spark = get_spark_session()
-    pr_df, cat_df, links_df = get_test_dataframes(spark)
-    prod_cat_df, prod_without_cat_df = get_pairs(spark, pr_df, cat_df, links_df)
+    pr_df, cat_df, l_df = get_test_dataframes(spark)
+    prod_cat_df, prod_without_cat_df = get_pairs(pr_df, cat_df, l_df)
     print("Products with categories (each pair)")
     prod_cat_df.show()
     print("Products without categories")
